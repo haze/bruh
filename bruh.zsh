e749d846e6a0a7950867054f8d910a53aff9fed0
Haze Booth <isnt@haze.cool>
2019-12-02 22:21:40 -0500

    fixed dirty git tracker

--- bruh.zsh
+++ bruh.zsh
@@ -6,15 +6,15 @@ hl='%F{green}'
 reset='%F{fb_default_code}'
 
 function precmd() {
+  git_dirty=''
   if [ -d "$PWD/.git" ]
   then
     git_branch="$(git rev-parse --abbrev-ref HEAD) "
     git_shorthash="$hl$(git rev-parse --short HEAD)$reset "
     git_commit_age="$(bruh_lca $(date +%s) $(git show -s --format=%ct))"
-    if [[ $(git diff --stat) != '' ]]; then
+    if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
       git_dirty="$hl+$reset "
     else
-      git_dirty=''
     fi
   else
     git_branch=''
