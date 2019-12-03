f982dd4391d132a7ad5f32d5d6fa641fca5ef423
Haze Booth <isnt@haze.cool>
2019-12-03 18:11:17 -0500

    Add static binaries, README, LICENSE, fix path shortening on windows, add screenshot

--- bruh.zsh
+++ bruh.zsh
@@ -9,12 +9,16 @@ function precmd() {
   git_dirty=''
   if [ -d "$PWD/.git" ]
   then
-    git_branch="$(git rev-parse --abbrev-ref HEAD) "
-    git_shorthash="$hl$(git rev-parse --short HEAD)$reset "
-    git_commit_age="$(bruh_lca $(date +%s) $(git show -s --format=%ct))"
-    if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
-      git_dirty="$hl+$reset "
+    git_branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null) "
+    if [[ $(git rev-list --all --abbrev=0 --abbrev-commit | wc -l) != "       0" ]]; then
+      git_shorthash="$hl$(git rev-parse --short HEAD 2>/dev/null)$reset "
+      git_commit_age="$(bruh_lca $(date +%s) $(git show -s --format=%ct))"
     else
+      git_shorthash=''
+      git_commit_age=''
+    fi
+    if [[ $(git status --short) != "" ]]; then
+      git_dirty="$hl+$reset "
     fi
   else
     git_branch=''
--- collapsed_pwd.zig
+++ collapsed_pwd.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
+const os = @import("builtin").os;
 const mem = std.mem;
 
 fn shorten(allocator: *mem.Allocator, input: []const u8) ![]const u8 {
     const envMap = try std.process.getEnvMap(allocator);
     var buf = try std.Buffer.initCapacity(allocator, input.len);
     var start: usize = 0;
-    if (envMap.get("HOME")) |home| {
+    if (envMap.get("HOME")) |home| { // posix only
         if (mem.startsWith(u8, input, home)) {
             try buf.append("~/");
             start = home.len;
@@ -15,16 +16,21 @@ fn shorten(allocator: *mem.Allocator, input: []const u8) ![]const u8 {
     }
     const sepStr = &[_]u8{std.fs.path.sep};
     var it = mem.tokenize(input[start..], sepStr);
+    const cutOffLength = if (os == .windows) 5 else 4;
     while (it.next()) |part| {
         if (it.index < (it.buffer.len - it.delimiter_bytes.len)) {
-            if (part.len > 4) {
-                try buf.append(part[0..4]);
-                try buf.append("…");
+            if (part.len > cutOffLength) {
+                try buf.append(part[0..cutOffLength]);
+                if (os == .windows) {
+                    try buf.append("...");
+                } else try buf.append("…");
             } else {
                 try buf.append(part);
             }
             try buf.append(sepStr);
-        } else try buf.append(part);
+        } else {
+            try buf.append(part);
+        }
     }
     return buf.toOwnedSlice();
 }
