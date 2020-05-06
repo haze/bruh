da827bc8ff08433490d3f5a7536f505bc70478d5
Haze Booth <isnt@haze.cool>
2020-05-06 15:37:22 -0400

    Update

--- bruh.zsh-theme
+++ bruh.zsh-theme
@@ -1,1 +1,31 @@
-bruh.zsh
+# bruh theme
+
+# setup
+setopt prompt_subst
+hl='%F{green}'
+reset='%F{fb_default_code}'
+
+function precmd() {
+  git_dirty=''
+  if [ -d "$PWD/.git" ]
+  then
+    git_branch="$(git rev-parse --abbrev-ref HEAD 2>/dev/null) "
+    if [[ $(git rev-list --all --abbrev=0 --abbrev-commit | wc -l) != "       0" ]]; then
+      git_shorthash="$hl$(git rev-parse --short HEAD 2>/dev/null)$reset "
+      git_commit_age="$(bruh_lca $(date +%s) $(git show -s --format=%ct))"
+    else
+      git_shorthash=''
+      git_commit_age=''
+    fi
+    if [[ $(git status --short) != "" ]]; then
+      git_dirty="$hl+$reset "
+    fi
+  else
+    git_branch=''
+    git_shorthash=''
+    git_commit_age=''
+  fi
+}
+
+PROMPT=' $(bruh_cpwd) '
+RPROMPT='$git_dirty$git_branch$git_shorthash$git_commit_age'
--- collapsed_pwd.zig
+++ collapsed_pwd.zig
@@ -1,43 +1,49 @@
 const std = @import("std");
-const os = @import("builtin").os;
+const os = @import("builtin").os.tag;
 const mem = std.mem;
 
 fn shorten(allocator: *mem.Allocator, input: []const u8) ![]const u8 {
     const envMap = try std.process.getEnvMap(allocator);
-    var buf = try std.Buffer.initCapacity(allocator, input.len);
+    var buf = try std.ArrayList(u8).initCapacity(allocator, input.len);
     var start: usize = 0;
     if (envMap.get("HOME")) |home| { // posix only
         if (mem.startsWith(u8, input, home)) {
-            try buf.append("~/");
+            try buf.appendSlice("~/");
             start = home.len;
         } else {
-            try buf.append("/");
+            try buf.append('/');
         }
     }
-    const sepStr = &[_]u8{std.fs.path.sep};
-    var it = mem.tokenize(input[start..], sepStr);
+
+    var it = mem.tokenize(input[start..], &[_]u8{std.fs.path.sep});
     const cutOffLength = if (os == .windows) 5 else 4;
     while (it.next()) |part| {
         if (it.index < (it.buffer.len - it.delimiter_bytes.len)) {
             if (part.len > cutOffLength) {
-                try buf.append(part[0..cutOffLength]);
+                try buf.appendSlice(part[0..cutOffLength]);
                 if (os == .windows) {
-                    try buf.append("...");
-                } else try buf.append("…");
+                    try buf.appendSlice("...");
+                } else try buf.appendSlice("…");
             } else {
-                try buf.append(part);
+                try buf.appendSlice(part);
             }
-            try buf.append(sepStr);
+            try buf.append(std.fs.path.sep);
         } else {
-            try buf.append(part);
+            try buf.appendSlice(part);
         }
     }
     return buf.toOwnedSlice();
 }
 
 pub fn main() !void {
-    var buf: [1024 * 8]u8 = undefined;
-    const allocator = &std.heap.FixedBufferAllocator.init(&buf).allocator;
-    const cwd = try std.process.getCwdAlloc(allocator);
-    try (std.io.getStdOut().outStream().stream).print("{}\n", try shorten(allocator, cwd));
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = &arena.allocator;
+    const out = std.io.getStdOut().outStream();
+    const cwd = std.process.getCwdAlloc(allocator) catch |e| {
+        try out.print("? {}\n", .{e});
+        return;
+    };
+    try out.print("{}\n", .{ try shorten(allocator, cwd) });
 }
