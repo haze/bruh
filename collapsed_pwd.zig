815d35b3b90379409f7a8b69d4306dea62b173f0
haze <isnt@haze.cool>
2020-10-25 16:03:25 -0400

    Add alias file supporrt

--- collapsed_pwd.zig
+++ collapsed_pwd.zig
@@ -2,16 +2,59 @@ const std = @import("std");
 const os = @import("builtin").os.tag;
 const mem = std.mem;
 
+fn readAliasFile(allocator: *mem.Allocator, home_dir: ?[]const u8) !std.StringHashMap([]const u8) {
+    var map = std.StringHashMap([]const u8).init(allocator);
+    if (home_dir) |home| {
+        const home_dir_handle = try std.fs.cwd().openDir(home, .{});
+        const alias_file = try home_dir_handle.openFile(".bruh_aliases", .{});
+        var reader = alias_file.reader();
+        const alias_file_contents = try reader.readAllAlloc(allocator, (try alias_file.stat()).size);
+
+        var tok_iter = mem.tokenize(alias_file_contents, "\n");
+        while (tok_iter.next()) |line| {
+            if (mem.lastIndexOfScalar(u8, line, ':')) |split_idx| {
+                try map.put(line[0..split_idx], line[split_idx + 1 .. line.len]);
+            } else {
+                if (mem.lastIndexOfScalar(u8, line, '/')) |slash_idx| {
+                    try map.put(line, line[slash_idx + 1 .. line.len]);
+                }
+            }
+        }
+    }
+    return map;
+}
+
+const alias_highlight = "%F{blue}";
+const reset_code = "%F{fb_default_code}";
+
 fn shorten(allocator: *mem.Allocator, input: []const u8) ![]const u8 {
     const envMap = try std.process.getEnvMap(allocator);
     var buf = try std.ArrayList(u8).initCapacity(allocator, input.len);
+    var home: ?[]const u8 = null;
     var start: usize = 0;
-    if (envMap.get("HOME")) |home| { // posix only
-        if (mem.startsWith(u8, input, home)) {
+
+    if (envMap.get("HOME")) |captured_home| { // posix only
+        home = captured_home;
+        if (mem.startsWith(u8, input, captured_home)) {
             try buf.appendSlice("~/");
-            start = home.len;
+            start += captured_home.len;
         } else {
             try buf.append('/');
+        }
+    }
+
+    var alias_map = readAliasFile(allocator, home) catch |e| null;
+    if (alias_map) |map| {
+        var iter = map.iterator();
+        while (iter.next()) |entry| {
+            if (mem.startsWith(u8, input, entry.key)) {
+                start += entry.key.len;
+                try buf.appendSlice(alias_highlight);
+                try buf.appendSlice(entry.value);
+                try buf.appendSlice(reset_code);
+                try buf.append('/');
+                break;
+            }
         }
     }
 
@@ -32,11 +75,16 @@ fn shorten(allocator: *mem.Allocator, input: []const u8) ![]const u8 {
             try buf.appendSlice(part);
         }
     }
+
+    if (alias_map) |*map| {
+        map.deinit();
+    }
+
     return buf.toOwnedSlice();
 }
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    var arena = std.heap.ArenaAllocator.init(std.heap.c_allocator);
     defer arena.deinit();
 
     const allocator = &arena.allocator;
@@ -45,5 +93,5 @@ pub fn main() !void {
         try out.print("? {}\n", .{e});
         return;
     };
-    try out.print("{}\n", .{ try shorten(allocator, cwd) });
+    try out.print("{}\n", .{try shorten(allocator, cwd)});
 }
